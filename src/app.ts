f25aa97b7e54e004b624dd9bd0b7c9a90069da18
@wrubleski <lwrubleski@vertigo.com.br>
2022-08-14 10:39:14 -0300

    add swagger-ui

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,13 @@ import { connect } from './infrastructure/dbConnection';
 import cors from 'cors';
 import { errorHandler } from './helpers/middlewares/defaultErrorHandler';
 import logger from './helpers/logger';
+import swaggerUi from "swagger-ui-express"
+import YAML from 'yamljs'
+const swagger = YAML.load("./posts.swagger.yaml")
 
 const app: Express = express()
 app.use(cors())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagger));
 routes(app)
 
 app.use(errorHandler)

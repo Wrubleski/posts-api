2a941d24ea83b2ca8173483865b15998be6a42b1
@wrubleski <henrike.hip@live.com>
2022-08-15 23:03:14 -0300

    linting

--- test/controller/posts.spec.ts
+++ test/controller/posts.spec.ts
@@ -1,41 +1,41 @@
-import { populate } from "../../src/infrastructure/populateDatabase";
-import { connect, stop } from "../../src/infrastructure/dbConfig"
-import Posts from "../../src/models/posts";
-import { addPost, deletePost, getPostsById, listPosts, updatePost } from "../../src/controllers/posts";
+import { populate } from '../../src/infrastructure/populateDatabase';
+import { connect, stop } from '../../src/infrastructure/dbConfig';
+import Posts from '../../src/models/posts';
+import {
+  addPost,
+  deletePost,
+  getPostsById,
+  listPosts,
+  updatePost,
+} from '../../src/controllers/posts';
 
 const postPayload = {
-  title: "Lorem Ipsum",
-  body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
-  tags: [
-    "Finibus Bonorum",
-    "Voluptatum Deleniti"
-  ]
-}
+  title: 'Lorem Ipsum',
+  body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
+  tags: ['Finibus Bonorum', 'Voluptatum Deleniti'],
+};
 
 const missingTitlePayload = {
-  body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
-  tags: [
-    "Finibus Bonorum",
-    "Voluptatum Deleniti"
-  ]
-}
+  body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
+  tags: ['Finibus Bonorum', 'Voluptatum Deleniti'],
+};
 
-describe("Post Controller unit tests", () => {
+describe('Post Controller unit tests', () => {
   let id: string;
   beforeAll(async () => {
     // Setup a database before running tests. Using in-memory mongodb server.
-    await connect()
-    await populate(Posts)
-  })
+    await connect();
+    await populate(Posts);
+  });
 
   afterAll(async () => {
     // Stop the databse after running the tests
-    await stop()
-  })
+    await stop();
+  });
 
-  describe("addPost test", () => {
-    it("should fail to save with empty body", async () => {
-      const mReq = { body: {  } } as any;
+  describe('addPost test', () => {
+    it('should fail to save with empty body', async () => {
+      const mReq = { body: {} } as any;
       const mRes = {
         status: jest.fn().mockReturnThis(),
         send: jest.fn(),
@@ -43,15 +43,15 @@ describe("Post Controller unit tests", () => {
       const mNext = jest.fn();
       await addPost(mReq, mRes, mNext);
 
-      expect(mRes.status).toHaveBeenCalledWith(500)
+      expect(mRes.status).toHaveBeenCalledWith(500);
       expect(mRes.send).toHaveBeenCalledWith({
-        error: "Failed to save. Please try again later.",
-        from: "posts-api",
-        timestamp: expect.anything()
-      })
-    })
+        error: 'Failed to save. Please try again later.',
+        from: 'posts-api',
+        timestamp: expect.anything(),
+      });
+    });
 
-    it("should return 201 whem post is saved", async () => {
+    it('should return 201 whem post is saved', async () => {
       const mReq = { body: postPayload } as any;
       const mRes = {
         status: jest.fn().mockReturnThis(),
@@ -60,10 +60,10 @@ describe("Post Controller unit tests", () => {
       const mNext = jest.fn();
       await addPost(mReq, mRes, mNext);
 
-      expect(mRes.status).toHaveBeenCalledWith(201)
-    })
+      expect(mRes.status).toHaveBeenCalledWith(201);
+    });
 
-    it("should call next whem body is missing a required field", async () => {
+    it('should call next whem body is missing a required field', async () => {
       const mReq = { body: missingTitlePayload } as any;
       const mRes = {
         status: jest.fn().mockReturnThis(),
@@ -72,30 +72,29 @@ describe("Post Controller unit tests", () => {
       const mNext = jest.fn();
       await addPost(mReq, mRes, mNext);
 
-      expect(mNext).toHaveBeenCalled()
-    })
-  })
+      expect(mNext).toHaveBeenCalled();
+    });
+  });
 
-  describe("listPosts tests", () => {
-    it("should return five posts whem size query param is five", async () => {
-      const mReq = { body: {  }, query: { size: 5, index: 0 } } as any;
+  describe('listPosts tests', () => {
+    it('should return five posts whem size query param is five', async () => {
+      const mReq = { body: {}, query: { size: 5, index: 0 } } as any;
       const mRes = {
         status: jest.fn().mockReturnThis(),
         send: jest.fn(),
       } as any;
       const mNext = jest.fn();
       await listPosts(mReq, mRes, mNext);
-      expect(mRes.send).toHaveBeenCalled()
-      expect(mRes.send.mock.lastCall[0]).toHaveLength(5)
+      expect(mRes.send).toHaveBeenCalled();
+      expect(mRes.send.mock.lastCall[0]).toHaveLength(5);
       // setup for delete and getPostById test
-      id = mRes.send.mock.lastCall[0][0].id
-    })
+      id = mRes.send.mock.lastCall[0][0].id;
+    });
+  });
 
-  })
-
-  describe("getPostsById tests", () => {
-    it("should return 200 with valid id", async () => {
-      const mReq = { body: {  }, params: { id: id } } as any;
+  describe('getPostsById tests', () => {
+    it('should return 200 with valid id', async () => {
+      const mReq = { body: {}, params: { id: id } } as any;
       const mRes = {
         status: jest.fn().mockReturnThis(),
         send: jest.fn(),
@@ -103,12 +102,12 @@ describe("Post Controller unit tests", () => {
       const mNext = jest.fn();
       await getPostsById(mReq, mRes, mNext);
 
-      expect(mRes.status).toHaveBeenCalledWith(200)
-      expect(mRes.send).toHaveBeenCalled()
-    })
+      expect(mRes.status).toHaveBeenCalledWith(200);
+      expect(mRes.send).toHaveBeenCalled();
+    });
 
-    it("should fail to get and call next with invalid id", async () => {
-      const mReq = { body: {  }, params: { id: "" } } as any;
+    it('should fail to get and call next with invalid id', async () => {
+      const mReq = { body: {}, params: { id: '' } } as any;
       const mRes = {
         status: jest.fn().mockReturnThis(),
         send: jest.fn(),
@@ -116,11 +115,11 @@ describe("Post Controller unit tests", () => {
       const mNext = jest.fn();
       await getPostsById(mReq, mRes, mNext);
 
-      expect(mNext).toHaveBeenCalled()
-    })
+      expect(mNext).toHaveBeenCalled();
+    });
 
-    it("should return 404 whem triyng to get a non-existent post", async () => {
-      const mReq = { params: { id: "abcdef123456abcdef123456" } } as any;
+    it('should return 404 whem triyng to get a non-existent post', async () => {
+      const mReq = { params: { id: 'abcdef123456abcdef123456' } } as any;
       const mRes = {
         status: jest.fn().mockReturnThis(),
         send: jest.fn(),
@@ -128,14 +127,14 @@ describe("Post Controller unit tests", () => {
       const mNext = jest.fn();
       await getPostsById(mReq, mRes, mNext);
 
-      expect(mRes.status).toHaveBeenCalledWith(404)
-      expect(mRes.send).toHaveBeenCalled()
-    })
-  })
+      expect(mRes.status).toHaveBeenCalledWith(404);
+      expect(mRes.send).toHaveBeenCalled();
+    });
+  });
 
-  describe("updatePost tests", () => {
-    it("should fail to update with empty body and invalid id", async () => {
-      const mReq = { body: {  }, params: { id: "" } } as any;
+  describe('updatePost tests', () => {
+    it('should fail to update with empty body and invalid id', async () => {
+      const mReq = { body: {}, params: { id: '' } } as any;
       const mRes = {
         status: jest.fn().mockReturnThis(),
         send: jest.fn(),
@@ -143,16 +142,19 @@ describe("Post Controller unit tests", () => {
       const mNext = jest.fn();
       await updatePost(mReq, mRes, mNext);
 
-      expect(mRes.status).toHaveBeenCalledWith(404)
+      expect(mRes.status).toHaveBeenCalledWith(404);
       expect(mRes.send).toHaveBeenCalledWith({
-        error: "Not Found",
-        from: "posts-api",
-        timestamp: expect.anything()
-      })
-    })
+        error: 'Not Found',
+        from: 'posts-api',
+        timestamp: expect.anything(),
+      });
+    });
 
-    it("should update whem body and id are valid", async () => {
-      const mReq = { body: { ...postPayload, title: "foo" }, params: { id: id } } as any;
+    it('should update whem body and id are valid', async () => {
+      const mReq = {
+        body: { ...postPayload, title: 'foo' },
+        params: { id: id },
+      } as any;
       const mRes = {
         status: jest.fn().mockReturnThis(),
         send: jest.fn(),
@@ -160,15 +162,18 @@ describe("Post Controller unit tests", () => {
       const mNext = jest.fn();
       await updatePost(mReq, mRes, mNext);
 
-      expect(mRes.status).toHaveBeenCalledWith(200)
-      expect(mRes.send).toHaveBeenCalled()
-      expect(mRes.send.mock.lastCall[0].title).toBe("foo")
-    })
-  })
+      expect(mRes.status).toHaveBeenCalledWith(200);
+      expect(mRes.send).toHaveBeenCalled();
+      expect(mRes.send.mock.lastCall[0].title).toBe('foo');
+    });
+  });
 
-  describe("deletePost tests", () => {
-    it("should fail to delete with invalid id", async () => {
-      const mReq = { body: {  }, params: { id: "abcdef123456abcdef123456" } } as any;
+  describe('deletePost tests', () => {
+    it('should fail to delete with invalid id', async () => {
+      const mReq = {
+        body: {},
+        params: { id: 'abcdef123456abcdef123456' },
+      } as any;
       const mRes = {
         status: jest.fn().mockReturnThis(),
         send: jest.fn(),
@@ -176,16 +181,16 @@ describe("Post Controller unit tests", () => {
       const mNext = jest.fn();
       await deletePost(mReq, mRes, mNext);
 
-      expect(mRes.status).toHaveBeenCalledWith(404)
+      expect(mRes.status).toHaveBeenCalledWith(404);
       expect(mRes.send).toHaveBeenCalledWith({
-        error: "Not Found",
-        from: "posts-api",
-        timestamp: expect.anything()
-      })
-    })
+        error: 'Not Found',
+        from: 'posts-api',
+        timestamp: expect.anything(),
+      });
+    });
 
-    it("should return 204 to whem post is deleted", async () => {
-      const mReq = { body: {  }, params: { id: id } } as any;
+    it('should return 204 to whem post is deleted', async () => {
+      const mReq = { body: {}, params: { id: id } } as any;
       const mRes = {
         status: jest.fn().mockReturnThis(),
         send: jest.fn(),
@@ -193,8 +198,8 @@ describe("Post Controller unit tests", () => {
       const mNext = jest.fn();
       await deletePost(mReq, mRes, mNext);
 
-      expect(mRes.status).toHaveBeenCalledWith(204)
-      expect(mRes.send).toHaveBeenCalled()
-    })
-  })
-})
+      expect(mRes.status).toHaveBeenCalledWith(204);
+      expect(mRes.send).toHaveBeenCalled();
+    });
+  });
+});

0669bd0e3e4ccb1b2d51ba26d88a2463a796fdd1
@wrubleski <henrike.hip@live.com>
2022-08-15 22:44:08 -0300

    linting

--- src/helpers/middlewares/errorHandler.ts
+++ src/helpers/middlewares/errorHandler.ts
@@ -1,13 +1,13 @@
-import logger from "../logger";
-import { Request, Response } from "express";
-import { IError } from "../../dtos/error";
+import logger from '../logger';
+import { Request, Response } from 'express';
+import { IError } from '../../dtos/error';
 
 export const errorHandler = (err: Error, req: Request, res: Response) => {
-  logger.error(err.message, {method: req.method, statusCode: 500})
+  logger.error(err.message, { method: req.method, statusCode: 500 });
   const errorOutput: IError = {
     error: err.message,
-    from: "posts-api",
-    timestamp: Date.now()
-  }
-  res.status(500).send(errorOutput)
-}
+    from: 'posts-api',
+    timestamp: Date.now(),
+  };
+  res.status(500).send(errorOutput);
+};
--- src/models/posts.ts
+++ src/models/posts.ts
@@ -1,12 +1,12 @@
-import { model, Schema } from "mongoose";
-import { IPost } from "../dtos/post";
+import { model, Schema } from 'mongoose';
+import { IPost } from '../dtos/post';
 
 const postSchema = new Schema<IPost>({
-  title: {type: String, required: true},
-  body: {type: String, required: true},
-  tags: {type: [String], required: true}
-})
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+  tags: { type: [String], required: true },
+});
 
-const Posts = model<IPost>("posts", postSchema);
+const Posts = model<IPost>('posts', postSchema);
 
 export default Posts;

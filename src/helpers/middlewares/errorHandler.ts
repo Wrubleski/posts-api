828c3a87adb2f3de88dd4286f5e4e08fc06eff6a
@wrubleski <henrike.hip@live.com>
2022-08-16 09:26:01 -0300

    fix error handler

--- src/helpers/middlewares/errorHandler.ts
+++ src/helpers/middlewares/errorHandler.ts
@@ -1,12 +1,14 @@
 import logger from '../logger';
 import { Request, Response } from 'express';
 import { IError } from '../../dtos/error';
+import { NextFunction } from 'connect';
 
-export const errorHandler: (err: Error, req: Request, res: Response) => void = (
+export const errorHandler: (
   err: Error,
   req: Request,
   res: Response,
-) => {
+  next: NextFunction,
+) => void = (err: Error, req: Request, res: Response, next: NextFunction) => {
   logger.warn(err.message, { method: req.method, statusCode: 500 });
   const errorOutput: IError = {
     error: err.message,
